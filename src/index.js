b3b153cbdc67714edbf5e55d76b5ef38f49459cf
Lordkaito <cespedesisai88@gmail.com>
2021-09-21 21:11:07 +0200

    Add basic structure, style and basic js for the to-do-list

--- src/index.js
+++ src/index.js
@@ -1,18 +1,52 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
-import printMe from './print.js';
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
 
-  // Lodash now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const itemsContainer = document.querySelector('.items-container');
+const input = document.createElement('input');
+const inputCont = document.querySelector('.input');
+const title = document.querySelector('.title');
+const titleText = document.createElement('p');
+const deleteCont = document.querySelector('.delete');
+const deleteText = document.createElement('p');
 
-  btn.innerHTML = 'Click me and check the console!'
-  btn.onclick = printMe;
-  element.appendChild(btn);
-  return element;
-}
+input.type = 'text';
+input.setAttribute('placeholder', 'Add new task');
+inputCont.appendChild(input);
 
-document.body.appendChild(component());
+titleText.textContent = 'Demo';
+title.appendChild(titleText);
+
+deleteText.textContent = 'Delete';
+deleteCont.appendChild(deleteText);
+
+const items = [
+  {
+    description: 'Do the dishes',
+    completed: false,
+    id: 0,
+  },
+  {
+    description: 'Take out trash',
+    completed: false,
+    id: 1,
+  },
+  {
+    description: 'Wash up',
+    completed: false,
+    id: 2,
+  },
+];
+
+window.onload = () => {
+  items.forEach((item) => {
+    const div = document.createElement('div');
+    div.classList.add('task');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    const text = document.createElement('p');
+    text.textContent = item.description;
+    div.appendChild(checkbox);
+    div.appendChild(text);
+    itemsContainer.appendChild(div);
+  });
+};
